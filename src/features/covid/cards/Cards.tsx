0ce5b566d97d8c3cf695ac92fa22816c420dce3d
narutaku0914 <tkm.nrsw@gmail.com>
2022-07-20 22:26:16 +0900

    create cards

---
+++ src/features/covid/cards/Cards.tsx
@@ -0,0 +1,62 @@
+import { FC } from "react";
+import { MdLocalHospital } from "react-icons/md";
+import Countup from "react-countup";
+import { useSelector } from "react-redux";
+import { selectDaily } from "../covidSlice";
+import { Grid, CardContent, Typography, Card } from "@mui/material";
+import "./Cards.moudule.css";
+
+export const Cards: FC = () => {
+  const daily = useSelector(selectDaily);
+  return (
+    <div className="container">
+      <Grid container spacing={1} justifyContent="center">
+        <Grid item xs={12} md={3} component={Card} className="infected">
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              <MdLocalHospital>Infected persons</MdLocalHospital>
+            </Typography>
+            <Typography variant="h5">
+              <Countup
+                start={0}
+                end={daily[daily.length - 1].Confirmed}
+                duration={1.5}
+                separator=","
+              />
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item xs={12} md={3} component={Card} className="recovered">
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              <MdLocalHospital>Recovered persons</MdLocalHospital>
+            </Typography>
+            <Typography variant="h5">
+              <Countup
+                start={0}
+                end={daily[daily.length - 1].Recovered}
+                duration={1.5}
+                separator=","
+              />
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item xs={12} md={3} component={Card} className="recovered">
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              <MdLocalHospital>Dead persons</MdLocalHospital>
+            </Typography>
+            <Typography variant="h5">
+              <Countup
+                start={0}
+                end={daily[daily.length - 1].Deaths}
+                duration={1.5}
+                separator=","
+              />
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
